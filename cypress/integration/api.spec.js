0a4765be2a7e39e962c96855b29390f891c46781
Lucia Fernandes <mariapereira820@hotmail.com>
2022-04-07 11:19:20 +0100

    REST API testing with cypress

--- cypress/integration/api.spec.js
+++ cypress/integration/api.spec.js
@@ -12,4 +12,20 @@ describe('REST API tests with Cypress', () => {
         
     })
 
+    it('API Test - Validate Name Value', () => {
+        cy.request('https://pokeapi.co/api/v2/pokemon/25').as('pokemon')
+        cy.get('@pokemon').its('body').should('include', {name:'pikachu'}) 
+    })
+
+    it('API test -Validate Negative Status Code', () => {
+        cy.request({
+            method: 'GET',
+            url: 'https://pokeapi.co/api/v2/pokemon/1000',
+            failOnStatusCode: false
+
+        })
+        .as("pokemon")
+        cy.get('@pokemon').its('status').should('eq', 404)
+    })
+
 })
